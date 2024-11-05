d8531326bd5ff3c3f67d4b754bb037e72b9866b8
hig1002 <hig7463@naver.com>
2024-11-05 16:39:42 +0900

    feat : avg

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log(process.argv);
+#!/usr/bin/env node
+
+const lib = require("./lib");
+
+let command = process.argv[2]; //명령어
+
+let numbers = process.argv
+    .slice(3, process.argv.length)
+    .map((n) => parseFloat(n));
+
+if (numbers.some((n) => isNaN(n))) {
+    console.log("Some arguments are not numbers!");
+    process.exit(1);
+}
+        
+
+let result;
+switch (command) {
+    case "avg":
+        result = lib.avg(numbers);
+        break;
+    case "prime":
+        result = lib.prime(numbers);
+        break;
+    case "factorial":
+        result = lib.factorial(numbers);
+        break;
+    default:
+        console.log("Wrong command!");
+        process.exit(1);
+}
+
+console.log(result);
+
